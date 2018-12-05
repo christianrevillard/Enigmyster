2d6a27670806824cdfef36a69e2b34e89b9b3e55
Christian <chrrevil@hotmail.com>
2018-12-06 00:12:00 +0100

    Initial Checkin, site structure for an AspNet.Core Angular solution

---
+++ Controllers/RiddleController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Enigmyster.Models;
+using Enigmyster.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Enigmyster.Controllers
+{
+    [Route("api/[controller]")]
+    public class RiddleController : Controller
+    {
+
+        private IDataRepository<Riddle, long> _repository;
+        public RiddleController(IDataRepository<Riddle, long> repo)
+        {
+            _repository = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Riddle> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Riddle Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Riddle Riddle)
+        {
+            _repository.Add(Riddle);
+        }
+
+        [HttpPut]
+        public void Put([FromBody]Riddle Riddle)
+        {
+            _repository.Update(Riddle.Id, Riddle);
+        }
+
+        [HttpDelete("{id}")]
+        public long Delete(int id)
+        {
+            return _repository.Delete(id);
+        }
+    }
+}
---
+++ Migrations/20181205222252_InitialCreation.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Enigmyster.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Riddle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Solution = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Riddle", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Riddle");
+        }
+    }
+}
